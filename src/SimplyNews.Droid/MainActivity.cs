8bc47da3c39b72a3a426481560514f5e3643d454
Shammi Theodore <shammi.theodore@outlook.com>
2022-04-22 19:47:42 +0530

    Added AppCenter integration

--- src/SimplyNews.Droid/MainActivity.cs
+++ src/SimplyNews.Droid/MainActivity.cs
@@ -8,6 +8,9 @@ using MvvmCross.Platforms.Android.Views;
 using System;
 using SimplyNews.Core.ViewModels;
 using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace SimplyNews
 {
@@ -23,6 +26,8 @@ namespace SimplyNews
 
             try
             {
+                AppCenter.Start("f28f8278-0802-4348-b2ad-62222f737047", typeof(Analytics), typeof(Crashes));
+
                 var toolbar = (Toolbar)FindViewById(Resource.Id.topActionBar);
                 SetSupportActionBar(toolbar);
                 
