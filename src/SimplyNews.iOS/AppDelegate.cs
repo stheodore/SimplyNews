b1a0fa1c35b625b836f78abd61ac8e95ca4a7de2
Shammi Theodore <shammi.theodore@outlook.com>
2022-04-23 13:44:13 +0530

    Integrate with AppCenter

--- src/SimplyNews.iOS/AppDelegate.cs
+++ src/SimplyNews.iOS/AppDelegate.cs
@@ -1,12 +1,22 @@
 ﻿using Foundation;
 using MvvmCross.Platforms.Ios.Core;
 using SimplyNews.Core;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using UIKit;
 
 namespace SimplyNews.iOS
 {
     [Register(nameof(AppDelegate))]
     public class AppDelegate : MvxApplicationDelegate<Setup, App>
     {
+        public override void FinishedLaunching(UIApplication application)
+        {
+            AppCenter.Start("3e62df6b-48ca-4c43-8ad8-c116a9e19432", typeof(Analytics), typeof(Crashes));
+
+            base.FinishedLaunching(application);
+        }
     }
 }
 
